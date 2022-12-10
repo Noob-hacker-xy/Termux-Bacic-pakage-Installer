909cf9996720a8bb740c1fb271f261afc6239249
Noob-hacker-xy <114892015+Noob-hacker-xy@users.noreply.github.com>
2022-12-10 09:06:26 +0600

    Add files via upload

---
+++ run.py
@@ -0,0 +1,126 @@
+import os
+import sys
+import time
+os.system('clear')
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(5. / 100)
+   
+noob='''
+\033[1;32;40m╭━╮╱╭╮╱╱╱╱╱╭╮╱╱╱╱╭╮╱╱╱╱╱╱╱╭╮
+\033[1;32;40m┃┃╰╮┃┃╱╱╱╱╱┃┃╱╱╱╱┃┃╱╱╱╱╱╱╱┃┃
+\033[1;32;40m┃╭╮╰╯┣━━┳━━┫╰━╮╱╱┃╰━┳━━┳━━┫┃╭┳━━┳━╮╭╮╭┳╮╱╭╮
+\033[1;32;40m┃┃╰╮┃┃╭╮┃╭╮┃╭╮┣━━┫╭╮┃╭╮┃╭━┫╰╯┫┃━┫╭┻┻╋╋┫┃╱┃┃
+\033[1;32;40m┃┃╱┃┃┃╰╯┃╰╯┃╰╯┣━━┫┃┃┃╭╮┃╰━┫╭╮┫┃━┫┣━┳╋╋┫╰━╯┃
+\033[1;32;40m╰╯╱╰━┻━━┻━━┻━━╯╱╱╰╯╰┻╯╰┻━━┻╯╰┻━━┻╯╱╰╯╰┻━╮╭╯
+\033[1;32;40m╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
+\033[1;32;40m╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯
+'''
+print(noob)
+slowprint('\033[1;32;40mWelcome To Our New Tool Termux-Bacic-pakage Installer')
+hacker = ' \033[1;31;40m[\033[1;33;40m1\033[1;31;40m] \033[1;36;40mStart Basic package Insall \n \033[1;31;40m[\033[1;33;40m2\033[1;31;40m] \033[1;36;40mShow all Basic package \n \033[1;31;40m[\033[1;33;40m3\033[1;31;40m] \033[1;36;40mFollow on Facebook Account\n \033[1;31;40m[\033[1;33;40m4\033[1;31;40m] \033[1;36;40mFollow On Github Account \n \033[1;31;40m[\033[1;33;40m5\033[1;31;40m] \033[1;36;40mTool Exit \n'
+print(hacker)
+
+
+
+
+korim = input('\033[1;33;40mEnter number \033[1;32;40m>>>\033[1;33;40m ')
+if korim == '1':
+  os.system('clear')
+  print ('Allow the Button For Access the Storage in Termux')
+   
+  os.system('termux-setup-storage')
+  os.system ("apt upgrade -y")
+  os.system('pkg install python2')
+  os.system('pip install --upgrade pip')
+  os.system('pip install requests')
+  os.system('pip install mechanize')
+  os.system('pip2 install requests')
+  os.system('pip2 install mechanize')
+  os.system('pip2 install bs4')
+  os.system('pip install lolcat')
+  os.system('pip2 install lolcat')
+  os.system('pkg install wget')
+  os.system('pkg install php -y')
+  os.system('pkg install nano -y')
+  os.system('pkg installcurl -y')
+  os.system('pkg install tor -y')
+  os.system('pkg install openssh')
+  os.system('pkg install bash')
+  os.system ("apt install python-dev -y")
+  os.system('apt install ruby -y')
+  os.system('apt install perl -y')
+  os.system('apt install nmap -y')
+  os.system('apt install clang -y')
+  os.system('apt install jq -y')
+  os.system('apt install macchanger -y')
+  os.system('apt install tar -tar')
+  os.system('apt install zip -y')
+  os.system('apt install unzip -y')
+  os.system('apt install bmon -y')
+  os.system ("apt install proot -y")
+  os.system ("apt install java -y")
+  os.system ("apt install figlet -y")
+  os.system('xdg-open https://www.facebook.com/profile.php?id=100087012443746')
+  os.system('clear')
+  print(noob)
+  print('All Basic package Install successful\n')
+  input('\nPress the enter key to exit ')
+elif korim == '2':
+  os.system('clear')
+  print(noob)
+  slowprint('''
+[01]  termux-setup-storage
+[02]  apt upgrade -y
+[03]  pkg install python2
+[04]  pip install --upgrade pip
+[05]  pip install requests
+[06]  pip install mechanize
+[07]  pip2 install requests
+[08]  pip2 install mechanize
+[09]  pip2 install bs4
+[10]  pip install lolcat
+[11]  pip2 install lolcat
+[12]  pkg install wget
+[13]  pkg install php -y
+[14]  pkg install nano -y
+[15]  pkg installcurl -y
+[16]  pkg install tor -y
+[17]  pkg install openssh
+[18]  pkg install bash
+[19]  apt install python-dev -y
+[20]  apt install ruby -y
+[21]  apt install perl -y
+[22]  apt install nmap -y
+[23]  apt install clang -y
+[24]  apt install jq -y
+[25]  apt install macchanger -y
+[26]  apt install tar -tar
+[27]  apt install zip -y
+[28]  apt install unzip -y
+[28]  apt install bmon -y
+[29]  apt install proot -y
+[30]  apt install java -y
+[31]  apt install figlet -y
+  ''')
+  shopno = input('Enter To Main manu ')
+  if shopno == '':
+    os.system('python run.py')
+    
+    
+    
+    
+elif korim == '3':
+  os.system('xdg-open https://www.facebook.com/profile.php?id=100074760376240')
+
+
+
+
+elif korim == '4':
+  os.system('xdg-open https://www.github.com/Noob-hacker-xy')
+elif korim == '5':
+ slowprint('\033[1;32;40mThanks For Useing Our Tool........!!!')
+
+  
